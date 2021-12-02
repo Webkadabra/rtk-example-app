9f1b09680172674f9011c2f98f9153623de8a246
Sergii <devkadabra@gmail.com>
2021-12-02 18:48:14 +0200

    build: add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [/*'./src/!**!/!*.{js,jsx,ts,tsx}'*/],
+    darkMode: 'media',
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+};
